d245537ea90bfe422c5b12a44f41c22a5a4fc05e
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-09-01 16:50:56 -0300

    feat(component): create reusable UserCard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,8 +1,21 @@
+import { FaUserCircle } from "react-icons/fa";
 
-const UserCard = () => {
-  return (
-    <div>UserCard</div>
-  )
+interface UserCardProps {
+  index: number;
+  person: string;
 }
 
-export default UserCard
+const UserCard = ({ index, person }: UserCardProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <section className="flex items-center">
+        <FaUserCircle className="text-3xl mr-3 text-gray-500" />
+        <span>{person.name}</span>
+      </section>
+
+      <button>{person.following ? "Following" : "Follow"}</button>
+    </div>
+  );
+};
+
+export default UserCard;
