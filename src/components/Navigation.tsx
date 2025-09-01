efa9221ad9c668df7071564b8b375fada2308e25
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-09-01 15:36:01 -0300

    feat(navbar): implement navigation bar with links and styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,23 @@
-import { FaSearch } from "react-icons/fa"
+import { FaSearch, FaUserCircle } from "react-icons/fa";
 
 const Navigation = () => {
   return (
     <nav className="border-2 text-black border-gray-200 p-4 flex justify-between">
       <div className="flex items-center border-2 rounded-full px-4 py-2 max-w-md ml-[5rem]">
         <FaSearch />
-      </div>
-    </nav>
-  )
-}
 
-export default Navigation
+        <input
+          type="text"
+          placeholder="Search ... "
+          className="bg-transparent outline-nonw w-full"
+        />
+      </div>
+
+      <section className="flex items-center mr-[5rem]">
+        <FaUserCircle className="text-3xl mr-2"/>
+      </section>
+    </nav>
+  );
+};
+
+export default Navigation;
